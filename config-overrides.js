e1b974b60e98e23b87dd8525bec3b7a1f08eb1ce
VuongQuangTung <tunghugo12@gmail.com>
2024-09-04 15:51:25 +0700

    91. Cài đặt babel-plugin-module-resolver

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,7 @@
 
-module.exports = function override(config, env) {
-    //do stuff with the webpack config...
-    return config;
-}
+const { override, useBabelRc } = require("customize-cra");
+
+module.exports = override(
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useBabelRc()
+);
